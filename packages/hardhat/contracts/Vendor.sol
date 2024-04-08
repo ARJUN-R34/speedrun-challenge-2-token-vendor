26b2fea7d4221c3b6bece38fd7783cf920191a77
Arjun Sasikumar <itsarjn@gmail.com>
2024-04-08 12:12:42 +0530

    completed speedrun challenge 2

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,21 +1,49 @@
 pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+contract Vendor is Ownable {
+	uint256 public constant tokensPerEth = 100;
+	YourToken public yourToken;
 
-  YourToken public yourToken;
+	// event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+	event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+	event SellTokens(
+		address seller,
+		uint256 amountOfTokens,
+		uint256 amountOfETH
+	);
 
-  // ToDo: create a payable buyTokens() function:
+	constructor(address tokenAddress) {
+		yourToken = YourToken(tokenAddress);
+	}
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+	// ToDo: create a payable buyTokens() function:
+	function buyTokens() public payable {
+		require(msg.value > 0, "Value needs to be greater than 0");
+		uint256 tokenAmount = msg.value * tokensPerEth;
+		yourToken.transfer(msg.sender, tokenAmount);
+		emit BuyTokens(msg.sender, msg.value, tokenAmount);
+	}
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+	// ToDo: create a withdraw() function that lets the owner withdraw ETH
+	function withdraw() public onlyOwner {
+		payable(owner()).transfer(address(this).balance);
+	}
+
+	// ToDo: create a sellTokens(uint256 _amount) function:
+	function sellTokens(uint256 _amount) public {
+		require(_amount > 0, "Cannot sell 0 token");
+		require(
+			_amount <= yourToken.balanceOf(msg.sender),
+			"Insufficient amount to sell"
+		);
+		yourToken.transferFrom(msg.sender, address(this), _amount);
+		payable(msg.sender).transfer(_amount / 100);
+
+		emit SellTokens(msg.sender, _amount, _amount / 100);
+	}
 }
